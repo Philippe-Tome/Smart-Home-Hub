784581eb323c8a24469418903b2ae25754b23773
Philippe-Tome <philippe.tome@protonmail.com>
2020-02-28 19:07:21 +1100

    test passed

--- reviewed_test/app_with_thread_changes_with_gpio.py
+++ reviewed_test/app_with_thread_changes_with_gpio.py
@@ -4,8 +4,8 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import QThread, pyqtSignal
 import sys
 import time
-# import RPi.GPIO as GPIO
-# GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 #led pin red=12, green=16, blue=20
 
@@ -42,17 +42,17 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
         self.green_pin=16
         self.blue_pin=20
 
-        # self.push_button=15 # mechanical push button
+        self.push_button=15 # mechanical push button
 
 
-        # GPIO.setup(self.red_pin, GPIO.OUT)
-        # GPIO.setup(self.green_pin, GPIO.OUT)
-        # GPIO.setup(self.blue_pin, GPIO.OUT)
-        # GPIO.setup(self.push_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.red_pin, GPIO.OUT)
+        GPIO.setup(self.green_pin, GPIO.OUT)
+        GPIO.setup(self.blue_pin, GPIO.OUT)
+        GPIO.setup(self.push_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        # GPIO.output(self.red_pin, False)
-        # GPIO.output(self.green_pin, False)
-        # GPIO.output(self.blue_pin, False)
+        GPIO.output(self.red_pin, False)
+        GPIO.output(self.green_pin, False)
+        GPIO.output(self.blue_pin, False)
 
 
         self.button_1_state=False
@@ -66,61 +66,58 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
         # Thread
         self.gpio_thread = GpioThread() #instantiating the thread and assigning to a variable within self
         self.gpio_thread.signal.connect(self.gpio_running_thread) # connecting the thread to the method so the thread knows where to pass signals to
-        self.gpio_thread,signal2.connect(self.button_callback_from_thread)
+        self.gpio_thread.signal2.connect(self.button_callback_from_thread)
         self.gpio_thread.start() #start the thread
 
-
-
-        # GPIO.add_event_detect(self.push_button,GPIO.RISING,callback=self.button_callback) 
-        
-        # message = input("Press enter to quit\n\n")
 
     # We are miraculously in a different class and getting messages from our GpioThread. Yay! :P 
     def gpio_running_thread(self, data):
         print("gpio_running_thread %s" % data)
 
     # This is from the button_callback method in the thread
-    def self.button_callback_from_thread(self, data):
-        print("this is the result when the button is pressed = %s" % data)
+    def button_callback_from_thread(self, data):
+        # print("this is the result when the button is pressed = %s" % data)
+        print("Thread timer = %s" % data)
+        self.pushButton_4.setText("on")
 
 
 
     def button_callback(self, channel):
         print("Button was pushed!")
-        self.pushButton_4.setText("on")
+        # self.pushButton_4.setText("on")
 
 
     def toggleLight1(self):
         if self.button_1_state==False:
             self.button_1_state=True
-            self.pushButton.setText("on")
-            # GPIO.output(self.red_pin, True)
+            self.pushButton.setText("Red ON")
+            GPIO.output(self.red_pin, True)
         else:
             self.button_1_state=False
-            self.pushButton.setText("off")
-            # GPIO.output(self.red_pin, False)
+            self.pushButton.setText("Red OFF")
+            GPIO.output(self.red_pin, False)
         print("red button pressed")
 
     def toggleLight2(self):
         if self.button_2_state==False:
             self.button_2_state=True
-            self.pushButton_2.setText("on")
-            # GPIO.output(self.green_pin, True)
+            self.pushButton_2.setText("Green ON")
+            GPIO.output(self.green_pin, True)
         else:
             self.button_2_state=False
-            self.pushButton_2.setText("off")
-            # GPIO.output(self.green_pin, False)
+            self.pushButton_2.setText("Green OFF")
+            GPIO.output(self.green_pin, False)
         print("green button pressed")
 
     def toggleLight3(self):
         if self.button_3_state==False:
             self.button_3_state=True
-            self.pushButton_3.setText("on")
-            # GPIO.output(self.blue_pin, True)
+            self.pushButton_3.setText("Blue ON")
+            GPIO.output(self.blue_pin, True)
         else:
             self.button_3_state=False
-            self.pushButton_3.setText("off")
-            # GPIO.output(self.blue_pin, False)
+            self.pushButton_3.setText("Blue OFF")
+            GPIO.output(self.blue_pin, False)
         print("blue button pressed")
 
 
---
+++ reviewed_test/main_window.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 90, 161, 61))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(330, 90, 121, 61))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(80, 220, 151, 51))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(330, 220, 111, 71))
+        self.pushButton_4.setAutoFillBackground(False)
+        self.pushButton_4.setObjectName("pushButton_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.toolBar_2 = QtWidgets.QToolBar(MainWindow)
+        self.toolBar_2.setObjectName("toolBar_2")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar_2)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Red OFF"))
+        self.pushButton_2.setText(_translate("MainWindow", "Green OFF"))
+        self.pushButton_3.setText(_translate("MainWindow", "Blue OFF"))
+        self.pushButton_4.setText(_translate("MainWindow", "Push Button not pressed"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.toolBar_2.setWindowTitle(_translate("MainWindow", "toolBar_2"))
+
